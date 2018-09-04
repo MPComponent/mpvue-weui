942c15bb87e90758dd2e334cf82ca10c4e0c1d47
KuangPF <1633397286@qq.com>
2018-09-04 23:13:00 +0800

    feat:component slider support

--- example/pages/slider/index.vue
+++ example/pages/slider/index.vue
@@ -1,6 +1,8 @@
 <template>
   <page-content title="" desc="滑块，这里采用小程序原生的slider。">
-    <mp-slider></mp-slider>
+    <mp-slider step=5 showValue></mp-slider>
+    <mp-slider min=10 max=30 step=5 showValue blockSize=20 backgroundColor="#000000"></mp-slider>
+    <mp-slider value=10 showValue blockSize=20 @change="change" @changing="changing"></mp-slider>
   </page-content>
 </template>
 
@@ -11,6 +13,14 @@ export default {
   components: {
     pageContent,
     mpSlider
+  },
+  methods: {
+    change(e) {
+      console.log(e);
+    },
+    changing(e) {
+      console.log(e);
+    }
   }
 };
 </script>
