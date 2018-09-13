b399e4f6aef2dab569d3d041172bd0a9c8e7c404
KuangPF <1633397286@qq.com>
2018-09-13 23:59:19 +0800

    feat:component actionsheet done

---
+++ example/pages/actionsheet/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <page-content title="ActionSheet" desc="弹出式菜单，采用小程序原生的actionsheet">
+    <mp-button type="default" size="large" btnClass="mb15" @click="showActionsheet">show actionsheet</mp-button>
+    <mp-actionsheet ref="actionsheet" :actions="actions"></mp-actionsheet>
+  </page-content>
+</template>
+
+<script>
+import mpActionsheet from '../../../src/actionsheet';
+import pageContent from '../../components/page-content';
+export default {
+  data() {
+    return {
+      actions: ['A', 'B', 'C']
+    };
+  },
+  components: {
+    pageContent,
+    mpActionsheet
+  },
+  methods: {
+    showActionsheet() {
+      this.$refs.actionsheet.show();
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/actionsheet/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="actionsheet"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    actions: {
+      type: Array,
+      default: []
+    }
+  },
+  methods: {
+    show() {
+      wx.showActionSheet({
+        itemList: this.actions,
+        success: function (res) {
+          console.log(res.tapIndex);
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
