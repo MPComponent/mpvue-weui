1f8422e1aa5ef945fc2e602fc7561dd5c936b37d
KuangPF <1633397286@qq.com>
2018-09-16 18:44:29 +0800

    docs:init docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  base: '/mpvue-weui/',
+  dest: 'dist',
+  title: 'mpvue-weui',
+  description: 'Just playing around',
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: `/logo.png`
+    }]
+  ],
+  themeConfig: {
+    repo: 'MPComponent/mpvue-weui',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+    nav: [],
+    sidebar: []
+  }
+};
