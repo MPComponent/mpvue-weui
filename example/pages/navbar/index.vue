ffea41265dbc3000a22b7ec07c5ee589d94de5b0
KuangPF <1633397286@qq.com>
2018-09-23 13:36:03 +0800

    feat: add props(tabs and activeIndex) for navbar component

---
+++ example/pages/navbar/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <page-content title="navbar" desc="导航栏">
+    <mp-navbar :tabs="tabs" activeIndex=0></mp-navbar>
+  </page-content>
+</template>
+
+<script>
+import mpNavbar from '../../../src/navbar';
+import pageContent from '../../components/page-content';
+export default {
+  data() {
+    return {
+      tabs: ['选项一', '选项二', '选项三']
+    };
+  },
+  components: {
+    mpNavbar,
+    pageContent
+  }
+};
+</script>
+
+<style>
+</style>
