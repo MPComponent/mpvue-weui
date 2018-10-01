bcb39f9756e1a839c7a085bff8d7e0358e03a56d
KuangPF <1633397286@qq.com>
2018-10-01 16:14:43 +0800

    feat: loading component add showLoading propos

---
+++ example/pages/loading/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <page-content title="loading" desc="加载组件">
+    <mp-loading :showLoading="showLoading"></mp-loading>
+    <mp-button @click="showLoading" size="large">打开 Loading</mp-button>
+  </page-content>
+</template>
+
+<script>
+import mpLoading from '../../../src/loading';
+import mpButton from '../../../src/button';
+import pageContent from '../../components/page-content';
+export default {
+  data() {
+    return {
+      showLoading: false
+    };
+  },
+  components: {
+    mpLoading,
+    pageContent,
+    mpButton
+  },
+  methods: {
+    showLoading() {
+      this.showLoading = true;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
