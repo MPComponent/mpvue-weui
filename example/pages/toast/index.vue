16d96549296dc385e850556b329aba7e0dcf83eb
KuangPF <1633397286@qq.com>
2018-10-03 13:51:27 +0800

    feat: toast component support v-model

--- example/pages/toast/index.vue
+++ example/pages/toast/index.vue
@@ -1,16 +1,30 @@
 <template>
   <page-content title="Toast" desc="提示组件">
-    <mp-toast type="error"></mp-toast>
+    <mp-button @click="toastText">只显示文字提示</mp-button>
+    <mp-toast :type="toastType" v-model="showToast"></mp-toast>
   </page-content>
 </template>
 
 <script>
 import mpToast from '../../../src/toast';
 import pageContent from '../../components/page-content';
+import mpButton from '../../../src/button';
 export default {
+  data() {
+    return {
+      showToast: false,
+      toastType: 'default'
+    };
+  },
   components: {
     mpToast,
-    pageContent
+    pageContent,
+    mpButton
+  },
+  methods: {
+    toastText() {
+      this.showToast = true;
+    }
   }
 };
 </script>
