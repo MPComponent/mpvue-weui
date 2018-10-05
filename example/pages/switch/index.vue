3efa4ddb4d570f98a3fb01bd244e0665a5ff0de6
KuangPF <1633397286@qq.com>
2018-10-06 00:07:33 +0800

    feat: init switch demo

---
+++ example/pages/switch/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <page-content title="Switch" desc="开关选择器，使用原生的 switch 组件，支持数据双向绑定">
+    <mp-switch></mp-switch>
+  </page-content>
+</template>
+
+<script>
+import mpSwitch from '../../../src/switch';
+import pageContent from '../../components/page-content';
+export default {
+  components: {
+    pageContent,
+    mpSwitch
+  },
+  methods: {
+    change(e) {
+      console.log(e);
+    },
+    changing(e) {
+      console.log(e);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/switch/index.vue
+++ src/switch/index.vue
@@ -1,11 +1,13 @@
 <template>
-  
+  <div class="weui-switch-content">
+    <switch></switch>
+  </div>
 </template>
 
 <script>
 export default {
 
-}
+};
 </script>
 
 <style>
