1d6248dc6b5bf08270f5a962ff8418e1dc69d19c
KuangPF <1633397286@qq.com>
2018-10-06 00:44:58 +0800

    feat: switch support v-model

--- example/pages/switch/index.vue
+++ example/pages/switch/index.vue
@@ -1,6 +1,6 @@
 <template>
   <page-content title="Switch" desc="开关选择器，使用原生的 switch 组件，支持数据双向绑定">
-    <mp-switch></mp-switch>
+    <mp-switch v-model="switchValue" @change="switchChange"></mp-switch>
   </page-content>
 </template>
 
@@ -8,15 +8,17 @@
 import mpSwitch from '../../../src/switch';
 import pageContent from '../../components/page-content';
 export default {
+  data() {
+    return {
+      switchValue: true
+    };
+  },
   components: {
     pageContent,
     mpSwitch
   },
   methods: {
-    change(e) {
-      console.log(e);
-    },
-    changing(e) {
+    switchChange(e) {
       console.log(e);
     }
   }
--- src/switch/index.vue
+++ src/switch/index.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="weui-switch-content">
-    <switch></switch>
+    <switch :checked="switchValue" @change="change"></switch>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      switchValue: this.value
+    };
+  },
+  props: {
+    value: {
+      type: Boolean,
+      default: true
+    }
+  },
+  watch: {
+    value(val) {
+      this.switchValue = val;
+    },
+    switchValue(val) {
+      this.$emit('input', val);
+    }
+  },
+  methods: {
+    change(e) {
+      this.switchValue = e.mp.detail.value;
+      this.$emit('change', e.mp.detail.value);
+    }
+  }
 };
 </script>
 
 <style>
-
 </style>
