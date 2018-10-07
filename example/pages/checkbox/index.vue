2cfdb3c4cb611dc0f8c0458d153cb8ab9cfa6508
KuangPF <1633397286@qq.com>
2018-10-07 22:50:13 +0800

    feat: add checked fot list item

--- example/pages/checkbox/index.vue
+++ example/pages/checkbox/index.vue
@@ -7,14 +7,9 @@
     <div class="page__bd page__bd_spacing">
       <div class="checkbox-demo-category">单个复选框</div>
       <div class="checkbox-demo-block">
-        <div class="checkbox-demo-title">{{ checkbox1.title }} (最多选择{{ checkbox1.max }}项)</div>
-        <mpCheckbox :options="checkbox1.options" :value="checkbox1.value" :max="checkbox1.max" @change="checkbox1Change"></mpCheckbox>
-      </div>
-
-      <div class="checkbox-demo-category">多个复选框</div>
-      <div v-for="(checkbox, index) in checkboxArr" class="checkbox-demo-block" :key="index">
-        <div class="checkbox-demo-title">{{ checkbox.title }} (最多选择{{ checkbox.max }}项)</div>
-        <mpCheckbox :options="checkbox.options" :value="checkbox.value" :max="checkbox.max" @change="checkboxArrChange" :id="index"></mpCheckbox>
+        <div class="checkbox-demo-title">
+          <mpCheckbox :list="checkboxList" :value="value" @change="checkboxChange"></mpCheckbox>
+        </div>
       </div>
     </div>
   </div>
@@ -28,58 +23,32 @@ export default {
   },
   data() {
     return {
-      checkbox1: {
-        title: '选择动物',
-        options: ['cat', 'dog', 'rabbit', 'pig', 'horse'],
-        value: ['cat', 'dog'],
-        max: 3
-      },
-      checkboxArr: [
-        {
-          title: '选择饮料',
-          options: ['water', 'milk', 'coffee', 'beer'],
-          value: ['water', 'coffee'],
-          max: 2
-        },
-        {
-          title: '选择水果',
-          options: ['apple', 'orange', 'banana', 'grape', 'pear'],
-          value: ['banana', 'pear'],
-          max: 3
-        }
-      ]
+      checkboxList: [{ value: 'cat' }, { value: 'dog' }, { value: 'rabbit' }, { value: 'pig' }, { value: 'horse' }],
+      value: ['dog', 'pig']
     };
   },
   methods: {
-    checkbox1Change(e) {
-      this.checkbox1.value = e.target.value;
-      console.log(this.checkbox1);
-    },
-    checkboxArrChange(e) {
-      let target = e.target;
-      this.checkboxArr[target.id].value = target.value;
-      console.log(this.checkboxArr);
+    checkboxChange(e) {
+      // console.log(e);
     }
   }
 };
 </script>
 
 <style scoped>
-
-  .checkbox-demo-category{
-    padding: 30px 0 0px;
-  }
-  .checkbox-demo-category:first-child{
-    padding-top: 0;
-  }
-  .checkbox-demo-title{
-    border-bottom: solid #d9d9d9 1px;
-    padding-top: 20px;
-    padding-bottom: 5px;
-  }
-  .checkbox-demo-block{
-    font-size: 15px;
-    border-bottom: solid #d9d9d9 1px;
-  }
-
+.checkbox-demo-category {
+  padding: 30px 0 0px;
+}
+.checkbox-demo-category:first-child {
+  padding-top: 0;
+}
+.checkbox-demo-title {
+  border-bottom: solid #d9d9d9 1px;
+  padding-top: 20px;
+  padding-bottom: 5px;
+}
+.checkbox-demo-block {
+  font-size: 15px;
+  border-bottom: solid #d9d9d9 1px;
+}
 </style>
--- src/checkbox/index.vue
+++ src/checkbox/index.vue
@@ -1,62 +1,58 @@
 <template>
-    <checkbox-group @change="checkboxChange" :id="id">
-        <label  v-for="(item, index) in optionsRender" class="weui-checkbox" :key="index">
-          <checkbox class="weui-checkbox-input" :value="item.value||item.name" type="checkbox" :checked="item.checked" :disabled="item.disabled" />
-          <div class="weui-checkbox-icon">
-            <icon v-show="item.checked!==true" class="weui-checkbox-icon-circle" type="circle" size="23"></icon>
-            <icon  v-show="item.checked===true" class="weui-checkbox-icon-success" type="success" size="23"></icon>
-          </div>
-          <div class="weui-checkbox-bd">{{item.name}}</div>
-        </label>
-    </checkbox-group>
+  <checkbox-group @change="checkboxChange" id="1">
+    <label v-for="(item, index) in listRender" class="weui-checkbox" :key="index">
+      <checkbox class="weui-checkbox-input" :value="item.value" type="checkbox" :checked="item.checked" />
+      <div class="weui-checkbox-icon">
+        <icon v-show="item.checked!==true" class="weui-checkbox-icon-circle" type="circle" size="23"></icon>
+        <icon v-show="item.checked===true" class="weui-checkbox-icon-success" type="success" size="23"></icon>
+      </div>
+      <div class="weui-checkbox-bd">{{item.value}}</div>
+    </label>
+  </checkbox-group>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      checkBoxValue: this.value
+    };
+  },
   props: {
-    options: {
-      type: Array,
-      required: true
-    },
     value: {
       type: Array,
-      default: []
+      default: () => []
     },
-    max: {
-      type: Number,
-      default: 0
-    },
-    id: ''
+    list: {
+      type: Array,
+      required: true
+    }
   },
   computed: {
-    optionsRender() {
-      let renderData = this.options.map((item) => {
-        if (!item.value) {
-          item = {
-            value: item,
-            name: item
-          };
-        }
-        item.checked = false;
-        item.disabled = false;
-        let judge = this.value.filter((val) => {
-          return val === item.value;
-        });
-
-        if (judge.length > 0) {
-          item.checked = true;
-        } else {
-          if (this.max > 0 && this.value.length >= this.max) {
-            item.disabled = true;
+    listRender() {
+      let length = this.checkBoxValue.length;
+      this.list.map(item => {
+        if (length > 0) {
+          for (let i = 0; i < this.checkBoxValue.length; i++) {
+            if (this.checkBoxValue[i] === item.value) {
+              item.checked = true;
+              console.log('sss');
+            } else {
+              item.checked = false;
+            }
           }
+        } else {
+          item.checked = false;
         }
-        return item;
       });
-      return renderData;
+      console.log(this.list);
+      return this.list;
     }
   },
   methods: {
     checkboxChange(e) {
+      console.log(e);
+      this.checkBoxValue = e.mp.detail.value;
       this.$emit('change', e);
     }
   }
@@ -64,53 +60,53 @@ export default {
 </script>
 
 <style>
-  .weui-checkbox{
-    padding: 10px 15px;
-    position: relative;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-box-align: center;
-    -webkit-align-items: center;
-    align-items: center;
-  }
+.weui-checkbox {
+  padding: 10px 15px;
+  position: relative;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
 
-  .weui-checkbox:active {
-    background-color: #ececec;
-  }
-  .weui-checkbox:first-child:before {
-    display: none;
-  }
-  .weui-checkbox:before {
-    content: " ";
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    height: 1px;
-    border-top: 1rpx solid #d9d9d9;
-    color: #d9d9d9;
-    left: 15px;
-  }
+.weui-checkbox:active {
+  background-color: #ececec;
+}
+.weui-checkbox:first-child:before {
+  display: none;
+}
+.weui-checkbox:before {
+  content: ' ';
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  height: 1px;
+  border-top: 1rpx solid #d9d9d9;
+  color: #d9d9d9;
+  left: 15px;
+}
 
-  .weui-checkbox-input {
-    position: absolute;
-    left: -9999px;
-  }
+.weui-checkbox-input {
+  position: absolute;
+  left: -9999px;
+}
 
-  .weui-checkbox-icon {
-    padding-right: 0.35em;
-  }
+.weui-checkbox-icon {
+  padding-right: 0.35em;
+}
 
-  .weui-checkbox-icon-circle,
-  .weui-checkbox-icon-success {
-    margin-left: 4.6px;
-    margin-right: 4.6px;
-  }
+.weui-checkbox-icon-circle,
+.weui-checkbox-icon-success {
+  margin-left: 4.6px;
+  margin-right: 4.6px;
+}
 
-  .weui-checkbox-bd {
-    -webkit-box-flex: 1;
-    -webkit-flex: 1;
-    flex: 1;
-  }
+.weui-checkbox-bd {
+  -webkit-box-flex: 1;
+  -webkit-flex: 1;
+  flex: 1;
+}
 </style>
