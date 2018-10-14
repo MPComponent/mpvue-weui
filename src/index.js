165c0b134e20d972fc5dc3136907a5e2ce915ebb
KuangPF <1633397286@qq.com>
2018-10-14 15:49:17 +0800

    feat: add component index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import mpButton from './button';
+import mpSlider from './slider';
+import mpCheckbox from './checkbox';
+import mpUploader from './uploader';
+import mpSwitch from './switch';
+
 import mpBadge from './badge';
+import mpGrid from './grid';
+import mpRate from './rate';
+import mpLoading from './loading';
+import mpToast from './toast';
+
+import mpPicker from './picker';
+import mpCityPicker from './city-picker';
+import mpDatePicker from './date-picker';
+import mpActionsheet from './actionsheet';
+import mpModal from './modal';
+
+import mpNavbar from './navbar';
+
+import mpSearchbar from './searchbar';
 
 export {
   mpButton,
-  mpBadge
+  mpSlider,
+  mpCheckbox,
+  mpUploader,
+  mpSwitch,
+  mpBadge,
+  mpGrid,
+  mpRate,
+  mpLoading,
+  mpToast,
+  mpPicker,
+  mpCityPicker,
+  mpDatePicker,
+  mpActionsheet,
+  mpModal,
+  mpNavbar,
+  mpSearchbar
 };
-
-module.exports = {
-  mpButton,
-  mpBadge
-};
-
-module.exports.default = module.exports;
