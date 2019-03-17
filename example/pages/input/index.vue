7fba9af8ef928728791b7d5f0c71735daba0651a
KuangPF <1633397286@qq.com>
2019-03-17 22:33:51 +0800

    feat: input component support v-model

---
+++ example/pages/input/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <page-content title="Input" desc="输入框，使用原生的 input 组件，支持数据双向绑定。">
+      <mp-input v-model="value" @input="input" :type="type"></mp-input>
+      
+    </page-content>
+  </div>
+</template>
+
+<script>
+import mpInput from '../../../src/input';
+import pageContent from '../../components/page-content';
+export default {
+  data() {
+    return {
+      value: 'test-input',
+      type: 'number'
+    };
+  },
+  components: {
+    mpInput,
+    pageContent
+  },
+  methods: {
+    input(value) {
+      console.log(`example:${this.value}`);
+    }
+  }
+};
+</script>
--- src/input/index.vue
+++ src/input/index.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="weui-input-content">
-   <input />
+    <input class="weui-input" :value="inputValue" @input="input" :type="type"/>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      inputValue: this.value
+    };
+  },
   props: {
-    
+    value: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    }
   },
   methods: {
-    
+    input(e) {
+      this.inputValue = e.mp.detail.value;
+      this.$emit('input', this.inputValue);
+      this.$emit('change', this.inputValue);
+    }
   }
 };
 </script>
 
 <style>
-
 </style>
