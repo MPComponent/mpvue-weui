23fef8d76bb07a915b1607fd1b16636b71d8121a
KuangPF <1633397286@qq.com>
2019-03-17 21:44:18 +0800

    feat: init input component

---
+++ src/input/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="weui-input-content">
+   <input />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style>
+
+</style>
