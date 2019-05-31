038cb651219e21eca9a317e84f0467502b1d947e
KuangPF <1633397286@qq.com>
2019-05-31 09:40:43 +0800

    docs: datePicker support themeColor

--- example/pages/date-picker/index.vue
+++ example/pages/date-picker/index.vue
@@ -3,7 +3,7 @@
     <page-content title="datepicker" desc="日期选择器，基于 picker 组件进行封装。">
       <div class="mpvue-date-picker">
         <button type="default" @click="showDatePicker">日期选择</button>
-        <mp-datepicker ref="mpDatePicker" :defaultDate="defaultDate" @onChange="onChange" @onConfirm="onConfirm" @onCancel="onCancel"></mp-datepicker>
+        <mp-datepicker ref="mpDatePicker" :themeColor="themeColor" :defaultDate="defaultDate" @onChange="onChange" @onConfirm="onConfirm" @onCancel="onCancel"></mp-datepicker>
       </div>
     </page-content>
     <div class="picker-text-content">
@@ -18,8 +18,9 @@ import pageContent from '../../components/page-content';
 export default {
   data() {
     return {
-      // defaultDate: new Date('2019-3-24'),
-      defaultDate: [2019, 2, 25],
+      defaultDate: new Date('2019-3-24'),
+      // defaultDate: [2019, 2, 25],
+      themeColor: '#2B7489',
       datePickerObj: {}
     };
   },
