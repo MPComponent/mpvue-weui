f77d7b2c3bc25434238da913153d6e4af2db120d
KuangPF <1633397286@qq.com>
2019-06-05 22:33:10 +0800

    docs(uploader): add clearFiles api

--- example/pages/uploader/index.vue
+++ example/pages/uploader/index.vue
@@ -1,12 +1,9 @@
 <template>
   <page-content title="uploader" desc="图片上传组件，基于 `wx.chooseImage` 和 `wx.previewImage` 实现。">
-    <div>
-      <mp-uploader ref="uploader" @upLoadSuccess="upLoadSuccess" @upLoadFail="upLoadFail" @upLoadComplete="upLoadComplete" @uploadDelete="uploadDelete" :showTip=false :count=2></mp-uploader>
-    </div>
+    <mp-uploader ref="uploader" @upLoadSuccess="upLoadSuccess" @upLoadFail="upLoadFail" @upLoadComplete="upLoadComplete" @uploadDelete="uploadDelete" :showTip=false :count=2></mp-uploader>
     <div class="clear-files">
       <mp-button size='small' @click="clearFiles">清空已上传文件</mp-button>
     </div>
-
   </page-content>
 </template>
 
@@ -41,7 +38,4 @@ export default {
 </script>
 
 <style>
-.clear-files {
-  margin-top: 10px;
-}
 </style>
